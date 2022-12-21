a952f1851304f96ba26005428dc3e977853fa95d
Ivonne Maldonado <ivonne.maldonadof@epn.edu.ec>
2022-12-21 15:32:19 -0500

    Se agrega el proyecto con el CRUD completo

---
+++ resources/views/posts/_form.blade.php
@@ -0,0 +1,20 @@
+@csrf
+<label class="uppercase text-gray-700 text-xs">Titulo</label>
+<span> @error('title') {{$message}} @enderror </span>
+<input type="text" name="title" class="rounded border-gray-200 w-full mb-4"  
+value="{{ old('title', $post->title) }}">
+
+<label class="uppercase text-gray-700 text-xs">URL</label>
+<span> @error('slug') {{$message}} @enderror </span>
+<input type="text" name="slug" class="rounded border-gray-200 w-full mb-4"  
+value="{{ old('slug', $post->slug) }}">
+
+<label class="uppercase text-gray-700 text-xs">Contenido</label>
+<span> @error('body') {{$message}} @enderror </span>
+<textarea name="body" class="rounded border-gray-200 w-full mb-4" 
+rows="5">{{ old('body', $post->body) }}</textarea> 
+<div class="flex justify-between items-center">
+    <a href="{{ route('posts.index') }}" class="text-indigo-600">Volver</a>
+
+    <input type="submit" value="Enviar" class="bg-gray-800 text-white rounded px-4 py-2">
+</div>
